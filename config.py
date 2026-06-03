26930bb13fcf33b4804e88947d0081fa09815218
darkhackerteampakistan <darkhackerteampakistan@gmail.com>
2026-06-03 18:22:29 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# ==========================
+# Facebook Page Manager
+# config.py
+# ==========================
+
+# Meta Graph API Version
+GRAPH_VERSION = "v23.0"
+
+# Base URL
+BASE_URL = f"https://graph.facebook.com/{GRAPH_VERSION}"
+
+# Page Access Token
+ACCESS_TOKEN = "EAAO9AmflYjoBRsonofHzZABwGqHZBLzOzMHgjoYBn2MX9RUvxOUY2hdLIlMcKJ2Jfqao37sE7wUsTPWW3wmUjgbcRim5bpTnua3qxaadFjRYTE8eZCgIuHULIJZCSAcGdiUrnD1xqoAe0rAVn71QMAbYgynWo12b4zZAvd6KNifxq22VIWSgkvFBGPA9XrbDYIoB8NO344GZB4FR1ZCTWwoVJTEbYquozTLzNhGxAZDZD"
+
+# Page ID
+PAGE_ID = "122142519662997957"
+
+# Request Timeout
+REQUEST_TIMEOUT = 30
+
+# Settings
+DEBUG = False
